7be07240d702df8f8634d0100ef872c2765f9c80
Johanna Mayer <johanna.mayer@student.uibk.ac.at>
2025-04-03 17:30:37 +0200

    loads links ans Ende

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ async function loadSights(url) {
         attribution: "Datenquelle: <a href ='https://data.wien.gv.at'>Stadt Wien</a>"
     }).addTo(map);
 }
-loadSights("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json");
+
 
 //Sightseeing
 async function loadLines(url) {
@@ -45,7 +45,7 @@ async function loadLines(url) {
         attribution: "Datenquelle: <a href ='https://data.wien.gv.at'>Stadt Wien</a>"
     }).addTo(map);
 }
-loadLines("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKLINIEVSLOGD&srsName=EPSG:4326&outputFormat=json");
+
 
 //Haltestellen
 async function loadStops(url) {
@@ -57,7 +57,7 @@ async function loadStops(url) {
         attribution: "Datenquelle: <a href ='https://data.wien.gv.at'>Stadt Wien</a>"
     }).addTo(map);
 }
-loadStops("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKHTSVSLOGD&srsName=EPSG:4326&outputFormat=json");
+
 
 //Fußgängerzone
 async function loadZones(url) {
@@ -69,4 +69,9 @@ async function loadZones(url) {
         attribution: "Datenquelle: <a href ='https://data.wien.gv.at'>Stadt Wien</a>"
     }).addTo(map);
 }
+
+///GeoJSON laden und visualisieren
+loadSights("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json");
+loadLines("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKLINIEVSLOGD&srsName=EPSG:4326&outputFormat=json");
+loadStops("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKHTSVSLOGD&srsName=EPSG:4326&outputFormat=json");
 loadZones("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:FUSSGEHERZONEOGD&srsName=EPSG:4326&outputFormat=json");
